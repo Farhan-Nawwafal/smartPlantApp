5562deee4038a0a1b2316bf5e81dbc908d8df8f0
Farhan-Nawwafal <farhan091204@gmail.com>
2025-02-10 11:21:54 +0700

    Fix Project

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -6,8 +6,8 @@ router.get('/', getDataController.getAllDatas);
 
 // Route baru untuk mengambil data dalam format JSON
 router.get('/getLatestData', getDataController.getLatestData);
-
-
 // Route baru untuk mengambil data dalam format JSON
+
+
 
 module.exports = router;
